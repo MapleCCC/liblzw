a6fbc750a8334b942eda3e7cb63bddbe0282f349
MapleCCC <littlelittlemaple@gmail.com>
2020-04-10 23:00:32 +0800

    Update integrate testing script

--- test_lzw.py
+++ test_lzw.py
@@ -1,59 +1,116 @@
 import os
-import random
-import string
+import shutil
 import subprocess
-from tempfile import TemporaryDirectory
+import uuid
+from pathlib import Path
+from random import sample
+from typing import List
 
-import pytest
-from hypothesis import given
-from hypothesis.strategies import integers
+from hypothesis import example, given, settings
+from hypothesis.strategies import binary, builds, lists
 
-VALID_CHARSET = (
-    string.ascii_letters + string.digits + string.punctuation + string.whitespace
+from LZW.__main__ import lzw_compress, lzw_decompress
+from LZW.utils import ascii2byte, is_equal_file
+
+EXAMPLE_EXE = os.path.join(os.getcwd(), "lzw_example_win.exe")
+# EXPERIMENT_EXE = os.path.join(os.getcwd(), "lzw.exe")
+EXPERIMENT_EXE = os.path.join(os.getcwd(), "lzw")
+BASELINE_EXE = os.path.join(os.getcwd(), "LZW", "__main__.py")
+
+MAX_TEST_FILE_LEN = 10000
+MAX_NUM_TEST_FILES = 3  # TODO: increase number of test files
+
+
+TEST_FILES_BUILD_STRATEGY = lists(
+    binary(max_size=MAX_TEST_FILE_LEN), min_size=1, max_size=MAX_NUM_TEST_FILES,
 )
 
-MAX_FILE_LEN = 7000
-MAX_NUM_TEST_FILES = 10
-
-BASELINE_EXE = os.path.join(os.getcwd(), "lzw_example_win.exe")
-EXPERIMENT_EXE = os.path.join(os.getcwd(), "lzw.exe")
-
-
-def read_file_content(filename: str) -> str:
-    with open(filename, "r", encoding="utf-8") as f:
-        return f.read()
+# All possible one-length bytes
+VALID_CHARSET = [ascii2byte(i) for i in range(256)]
+OVERFLOW_TEST_EXAMPLE_STRATEGY = builds(
+    lambda: b"".join(b"".join(sample(VALID_CHARSET, k=256)) for _ in range(20))
+)
+EXAMPLE_TEXT_TEST_CODE_DICT_OVERFLOW = b"".join(
+    b"".join(sample(VALID_CHARSET, k=256)) for _ in range(20)
+)
 
 
-def diff_file(filename1: str, filename2: str) -> int:
-    content1 = read_file_content(filename1)
-    content2 = read_file_content(filename2)
-    return content1 == content2
+@given(l=TEST_FILES_BUILD_STRATEGY)
+@example(l=[EXAMPLE_TEXT_TEST_CODE_DICT_OVERFLOW] * 3)
+@settings(deadline=None)
+def test_integration(l: List[bytes], tmp_path: Path) -> None:
+    # We need to intentionally create a unique subpath for each function invocation
+    # Because every hypothesis' example of the test function share the same
+    # tmp_path fixture instance, which is undesirable for some test cases.
+    subpath = tmp_path / str(uuid.uuid4())
+    subpath.mkdir()
+    os.chdir(subpath)
+
+    test_files = [f"file{i}" for i in range(len(l))]
+    for test_file, s in zip(test_files, l):
+        with open(test_file, "wb") as f:
+            f.write(s)
+
+    subprocess.run([EXPERIMENT_EXE, "-c", "a.lzw"] + test_files).check_returncode()
+
+    for test_file in test_files:
+        shutil.move(test_file, test_file + "old")
+
+    subprocess.run([EXPERIMENT_EXE, "-d", "a.lzw"]).check_returncode()
+
+    for test_file in test_files:
+        assert is_equal_file(test_file, test_file + "old", mode="rb")
 
 
-def generate_gibberish_file(filename: str) -> None:
-    content = "".join(random.choices(VALID_CHARSET, k=random.randrange(MAX_FILE_LEN)))
-    with open(filename, "w", encoding="utf-8") as f:
-        f.write(content)
+@given(l=TEST_FILES_BUILD_STRATEGY)
+@example(l=[EXAMPLE_TEXT_TEST_CODE_DICT_OVERFLOW] * 3)
+@settings(deadline=None)
+def test_compression(l: List[bytes], tmp_path: Path) -> None:
+    # We need to intentionally create a unique subpath for each function invocation
+    # Because every hypothesis' example of the test function share the same
+    # tmp_path fixture instance, which is undesirable for some test cases.
+    subpath = tmp_path / str(uuid.uuid4())
+    subpath.mkdir()
+    os.chdir(subpath)
+
+    test_files = [f"file{i}" for i in range(len(l))]
+    for test_file, s in zip(test_files, l):
+        with open(test_file, "wb") as f:
+            f.write(s)
+
+    subprocess.run([EXPERIMENT_EXE, "-c", "a.lzw"] + test_files).check_returncode()
+
+    for test_file in test_files:
+        shutil.move(test_file, test_file + "old")
+
+    lzw_decompress("a.lzw")
+
+    for test_file in test_files:
+        assert is_equal_file(test_file, test_file + "old", mode="rb")
 
 
-# There are different testing strategies. Our easy way is to set the random seed.
-@given(integers())
-def test_compression(seed: int) -> None:
-    random.seed(seed)
+@given(l=TEST_FILES_BUILD_STRATEGY)
+@example(l=[EXAMPLE_TEXT_TEST_CODE_DICT_OVERFLOW] * 3)
+@settings(deadline=None)
+def test_decompression(l: List[bytes], tmp_path: Path) -> None:
+    # We need to intentionally create a unique subpath for each function invocation
+    # Because every hypothesis' example of the test function share the same
+    # tmp_path fixture instance, which is undesirable for some test cases.
+    subpath = tmp_path / str(uuid.uuid4())
+    subpath.mkdir()
+    os.chdir(subpath)
 
-    with TemporaryDirectory() as directory:
-        os.chdir(directory)
+    test_files = [f"file{i}" for i in range(len(l))]
+    for test_file, s in zip(test_files, l):
+        with open(test_file, "wb") as f:
+            f.write(s)
 
-        test_files = [f"file{i}" for i in range(random.randrange(MAX_NUM_TEST_FILES))]
-        for test_file in test_files:
-            generate_gibberish_file(test_file)
+    lzw_compress("a.lzw", test_files)
 
-        subprocess.run([EXPERIMENT_EXE, "-c", "output.lzw"] + test_files)
-        subprocess.run([BASELINE_EXE, "-c", "output_example.lzw"] + test_files)
+    for test_file in test_files:
+        shutil.move(test_file, test_file + "old")
 
-        assert diff_file("output.lzw", "output_example.lzw")
+    subprocess.run([EXPERIMENT_EXE, "-d", "a.lzw"]).check_returncode()
 
-
-@pytest.mark.xfail(reason="Not Yet Implemented")
-def test_decompress():
-    raise NotImplementedError
+    for test_file in test_files:
+        assert is_equal_file(test_file, test_file + "old", mode="rb")
