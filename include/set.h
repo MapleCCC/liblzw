5e31d977125d657fae53b97f1d51c13524a13004
MapleCCC <littlelittlemaple@gmail.com>
2020-04-16 13:55:19 +0800

    Implement set data structure

--- include/set.h
+++ include/set.h
@@ -2,4 +2,41 @@
 
 #include "dict.h"
 
-// typedef Dict<T, NULL> Set<T>;
+class _dummy {};
+
+template <class T>
+class Set {
+   public:
+    void clear();
+    void reserve(unsigned);
+    void add(const T&);
+    bool contains(const T&) const;
+    // std::string str() const;
+
+   private:
+    Dict<T, _dummy> storage;
+};
+
+template <class T>
+inline void
+Set<T>::clear() {
+    storage.clear();
+}
+
+template <class T>
+inline void
+Set<T>::reserve(unsigned size) {
+    storage.reserve(size);
+}
+
+template <class T>
+inline void
+Set<T>::add(const T& elem) {
+    storage.set(elem, _dummy());
+}
+
+template <class T>
+inline bool
+Set<T>::contains(const T& elem) const {
+    return storage.contains(elem);
+}
