ca3989b9de3f098c5701b467e0903633023a6e70
MapleCCC <littlelittlemaple@gmail.com>
2020-04-18 23:26:12 +0800

    Refactor Bytes data structure implementation, prepare for implementing move semantics.

--- include/bytes.h
+++ include/bytes.h
@@ -9,41 +9,41 @@
 // data as ASCII text, although in some situations they are interchangeable. For
 // safety, it's best to avoid doing that.
 
+// TODO: implement move semantic to improve performance in many situations
+
 // Bytes type is merely a semantic thin abstraction over vector of unsigned
 // chars.
 class Bytes {
    public:
     Bytes();
-    Bytes(const unsigned char);
-    Bytes(const char*);
-    Bytes(const std::string);
-    unsigned char get(int index) const;
-    int length() const;
+    Bytes(unsigned char);
+    Bytes(const Bytes&);
+    ~Bytes();
+    unsigned char get(unsigned index) const;
+    unsigned length() const;
     unsigned char get_first_byte() const;
     void push_back(unsigned char c);
     long long hash() const;
+    Bytes& operator=(const Bytes&);
     bool operator==(const Bytes& rhs) const;
     bool operator!=(const Bytes& rhs) const;
     Bytes operator+(const Bytes& rhs) const;
     Bytes operator+(const unsigned char c);
     std::string str() const;
-    // friend std::string operator+(const std::string& s, const Bytes&
-    // bytes); friend std::ostream& operator<<(std::ostream& os, const
-    // Bytes& bytes);
 
    private:
-    std::vector<unsigned char> storage;
+    std::vector<unsigned char>* storage;
     long long hashcode;
 
+    void init();
     void update_hashcode(unsigned char);
 };
 
-std::string operator+(const std::string& s, const Bytes& bytes);
 std::ostream& operator<<(std::ostream& os, const Bytes& bytes);
 
-inline int
+inline unsigned
 Bytes::length() const {
-    return storage.size();
+    return storage->size();
 }
 
 #endif /* _BYTES_H_ */
