8042441a7d8c1bfd4f44f85ae6100e7b76ae990d
MapleCCC <littlelittlemaple@gmail.com>
2020-04-18 15:00:15 +0800

    Add entry point

---
+++ lzw.cpp
@@ -0,0 +1,12 @@
+// #include <lzw/cli.h>
+#include <iostream>
+
+#include "cli.h"
+
+using namespace std;
+
+int
+main(int argc, char** argv) {
+    int returncode = cli(argc, argv);
+    return returncode;
+}
