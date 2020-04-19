1bd3061fe572d001354dfa9bd95c71a49af1aea3
MapleCCC <littlelittlemaple@gmail.com>
2020-04-19 16:31:16 +0800

    Refactor: reimplement Bitarray in more efficient way

---
+++ include/bigint.h
@@ -0,0 +1,20 @@
+#ifndef _BIGINT_H_
+#define _BIGINT_H_
+
+// Currently a dirty and incomplete workaround
+typedef unsigned long long BigInt;
+
+#define MAX_BIG_INT_BIT_SIZE 8 * sizeof(BigInt)
+
+// #include <vector>
+
+// class BigInt {
+//    public:
+//     BigInt();
+//     operator<<();
+
+//    private:
+//     std::vector<unsigned> data;
+// };
+
+#endif /* _BIGINT_H_ */
