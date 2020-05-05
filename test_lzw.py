a48f3ec0b0c2211b891bc18bf033a2e850326c2a
MapleCCC <littlelittlemaple@gmail.com>
2020-05-05 17:32:37 +0800

    [test_lzw] No need to test against baseline Python implementation of LZW algorithm. Integrate test suffices.

--- test_lzw.py
+++ test_lzw.py
@@ -9,7 +9,6 @@ from typing import List
 from hypothesis import example, given, settings
 from hypothesis.strategies import binary, builds, lists
 
-from LZW.__main__ import lzw_compress, lzw_decompress
 from LZW.utils import ascii2byte, is_equal_file
 
 EXAMPLE_EXE = os.path.join(os.getcwd(), "lzw_example_win.exe")
@@ -52,64 +51,6 @@ def test_integration(l: List[bytes], tmp_path: Path) -> None:
             f.write(s)
 
     subprocess.run([EXPERIMENT_EXE, "-c", "a.lzw"] + test_files).check_returncode()
-
-    for test_file in test_files:
-        shutil.move(test_file, test_file + "old")
-
-    subprocess.run([EXPERIMENT_EXE, "-d", "a.lzw"]).check_returncode()
-
-    for test_file in test_files:
-        assert is_equal_file(test_file, test_file + "old", mode="rb")
-
-
-@given(l=TEST_FILES_BUILD_STRATEGY)
-@example(l=[EXAMPLE_TEXT_TEST_CODE_DICT_OVERFLOW] * 3)
-@settings(deadline=None)
-def test_compression(l: List[bytes], tmp_path: Path) -> None:
-    # We need to intentionally create a unique subpath for each function invocation
-    # Because every hypothesis' example of the test function share the same
-    # tmp_path fixture instance, which is undesirable for some test cases.
-    subpath = tmp_path / str(uuid.uuid4())
-    subpath.mkdir()
-    os.chdir(subpath)
-
-    test_files = [f"file{i}" for i in range(len(l))]
-    for test_file, s in zip(test_files, l):
-        with open(test_file, "wb") as f:
-            f.write(s)
-
-    subprocess.run([EXPERIMENT_EXE, "-c", "a.lzw"] + test_files).check_returncode()
-
-    lzw_compress("b.lzw", test_files)
-
-    assert is_equal_file("a.lzw", "b.lzw", mode="rb")
-
-    for test_file in test_files:
-        shutil.move(test_file, test_file + "old")
-
-    lzw_decompress("a.lzw")
-
-    for test_file in test_files:
-        assert is_equal_file(test_file, test_file + "old", mode="rb")
-
-
-@given(l=TEST_FILES_BUILD_STRATEGY)
-@example(l=[EXAMPLE_TEXT_TEST_CODE_DICT_OVERFLOW] * 3)
-@settings(deadline=None)
-def test_decompression(l: List[bytes], tmp_path: Path) -> None:
-    # We need to intentionally create a unique subpath for each function invocation
-    # Because every hypothesis' example of the test function share the same
-    # tmp_path fixture instance, which is undesirable for some test cases.
-    subpath = tmp_path / str(uuid.uuid4())
-    subpath.mkdir()
-    os.chdir(subpath)
-
-    test_files = [f"file{i}" for i in range(len(l))]
-    for test_file, s in zip(test_files, l):
-        with open(test_file, "wb") as f:
-            f.write(s)
-
-    lzw_compress("a.lzw", test_files)
 
     for test_file in test_files:
         shutil.move(test_file, test_file + "old")
