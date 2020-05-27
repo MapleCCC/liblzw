011b896b07a7d9b16752ffc0cd574f58987a8ab3
MapleCCC <littlelittlemaple@gmail.com>
2020-05-28 01:25:14 +0800

    Update unit tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -6,9 +6,6 @@
 #include "bytes.h"
 #include "code.h"
 #include "code_dict.h"
-#include "codec.h"
-#include "dict.tpp"
-#include "lzwfile.h"
 #include "str_dict.h"
 #include "utils.tpp"
 
@@ -16,26 +13,6 @@ using namespace std;
 
 // Add more test cases to cover as many interface as possible.
 // C++ test coverage calculation tool?
-
-void
-test_dict() {
-    Dict<Bytes, Code> d;
-    d.set("1", Code(10));
-    d.set(Bytes("10"), Code(100));
-    assert(d.get(Bytes("1")) == Code(10));
-    assert(d.contains(Bytes("1")));
-    assert(d.contains_value(10));
-    assert(d.contains_value(100));
-    assert(!d.contains_value(200));
-    assert(!d.contains(Bytes("2")));
-    assert(d.contains(Bytes("10")));
-    assert(d.get(Bytes("10")) == Code(100));
-    assert(d.length() == 2);
-    d.clear();
-    assert(d.length() == 0);
-
-    cout << "Pass test cases for Dict" << endl;
-}
 
 void
 test_code_dict() {
@@ -106,50 +83,11 @@ test_str_dict() {
 }
 
 void
-test_codec() {
-    LZWEncoder encoder(12);
-    LZWDecoder decoder(12);
+test_is_two_files_identical() {
+    assert(is_two_files_identical(".gitignore", ".gitignore"));
+    assert(!is_two_files_identical(".travis.yml", ".gitignore"));
 
-    vector<Code>* codes = encoder.encode_file("Ephesians.txt");
-    vector<Code>::iterator codes_itr = codes->begin();
-    decoder.decode_file("E.txt", codes_itr);
-    delete codes;
-
-    assert(is_two_files_identical("Ephesians.txt", "E.txt"));
-
-    cout << "Pass test cases for codec" << endl;
-}
-
-void
-test_utils() {
-    assert(is_two_files_identical("Ephesians.txt", "E.txt"));
-    assert(!is_two_files_identical("Ephesians.txt", ".gitignore"));
-
-    cout << "Pass test cases for utils" << endl;
-}
-
-void
-test_lzwfile() {
-    vector<string>* header = read_lzwfile_header("a.lzw");
-    for (int i = 0; i < header->size(); i++) {
-        cout << header->at(i) << endl;
-    }
-    write_lzwfile_header("b.lzw", header);
-    delete header;
-
-    vector<Code>* codes = read_lzwfile_codes("a.lzw", 12);
-    cout << "Print codes" << endl;
-    for (int i = 0; i < codes->size(); i++) {
-        cout << codes->at(i) << endl;
-    }
-    lzwfile_codes_writer writer("b.lzw", 12);
-    writer.write(codes);
-    writer.close();
-    delete codes;
-
-    assert(is_two_files_identical("a.lzw", "b.lzw"));
-
-    cout << "Pass test cases for lzwfile" << endl;
+    cout << "Pass test cases for is_two_files_identical" << endl;
 }
 
 void
@@ -163,33 +101,34 @@ test_grouper() {
     v.push_back(6);
     v.push_back(7);
     vector<vector<int> > v2 = grouper(v, 2);
-    for (int i = 0; i < v2.size(); i++) {
-        cout << "First subvector: " << endl;
+    vector<int> v3;
+    for (unsigned i = 0; i < v2.size(); i++) {
         vector<int> subv = v2.at(i);
-        for (int j = 0; j < subv.size(); j++) {
-            cout << subv.at(j) << " ";
-        }
-        cout << endl;
+        v3.insert(v3.end(), subv.begin(), subv.end());
     }
+    v3.push_back(7);
+    assert(v == v3);
+
+    cout << "Pass test cases for grouper" << endl;
 }
 
 void
 test_bitarray() {
     Bitarray b;
-    b.push_bytes_back((unsigned char)0);
-    Bytes byte = b.pop_byte_front();
+    b.push_bytes_back(Bytes(1, (unsigned char)0));
+    Bytes byte(1, b.pop_byte_front());
     assert(byte.length() == 1);
-    assert(byte.get(0) == 0);
+    assert(byte[0] == 0);
     assert(b.length() == 0);
-    b.push_bytes_back((unsigned char)0);
-    byte = b.pop_bytes_front();
+    b.push_bytes_back(Bytes(1, (unsigned char)0));
+    byte = Bytes(1, b.pop_byte_front());
     assert(byte.length() == 1);
-    assert(byte.get(0) == 0);
+    assert(byte[0] == 0);
     assert(b.length() == 0);
 
     b = Bitarray::from_int(0, 12);
     assert(b.length() == 12);
-    assert(b.pop_byte_front() == Bytes((unsigned char)0));
+    assert(b.pop_byte_front() == (unsigned char)0);
 
     cout << "Pass test cases for bitarray" << endl;
 }
@@ -197,27 +136,11 @@ test_bitarray() {
 int
 main() {
     try {
-        // string s = int2str(9);
-        // cout << s << endl;
-        // s = int2str(0);
-        // cout << s << endl;
-        // s = int2str(7);
-        // cout << s << endl;
-        // s = int2str(190);
-        // cout << s << endl;
-
-        // char cs[] = {'1', '\0'};
-        // string s(cs);
-        // cout << (s == "1") << endl;
-
-        // test_dict();
-        // test_code_dict();
-        // test_str_dict();
-        // test_utils();
-        // test_codec();
-        // test_bitarray();
-        // test_grouper();
-        test_lzwfile();
+        test_code_dict();
+        test_str_dict();
+        test_is_two_files_identical();
+        test_grouper();
+        test_bitarray();
 
         cout << "Pass all test cases!" << endl;
     } catch (exception e) {
--- tests/test_bitarray.cpp
+++ tests/test_bitarray.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int
 main() {
     Bitarray b;
-    b.push_bytes_back(Bytes('1'));
+    b.push_bytes_back(Bytes(1, '1'));
     unsigned char c = b.pop_byte_front();
     assert(c == 49);
 
